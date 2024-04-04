ab0cd40233c3d8bb8637b9c9a467570989a4d5b9
kotlj <crotlj6@gmail.com>
2024-04-04 22:33:37 +0300

    done

---
+++ HW_Client_2/HW_Client_2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_Client_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                TcpClient tcpClient = new TcpClient("127.0.0.1", 11111);
+
+                Stream stream = tcpClient.GetStream();
+                UTF8Encoding encoding = new UTF8Encoding();
+                byte[] byteSend = null;
+                byte[] byteRecv = new byte[1024];
+                int bytes = 0;
+
+                Console.WriteLine("Enter your name:\n");
+                string msg = Console.ReadLine();
+                byteSend = encoding.GetBytes(msg);
+                stream.Write(byteSend, 0, byteSend.Length);
+
+                while (true)
+                {
+                    Console.WriteLine("Enter what exchange rate you want to know(Example: UAH EURO)\nFor now we contain UAH, USD, EURO, KZT:\n");
+                    msg = Console.ReadLine();
+                    byteSend = encoding.GetBytes(msg);
+                    stream.Write(byteSend,0, byteSend.Length);
+
+                    bytes = stream.Read(byteRecv, 0, byteRecv.Length);
+                    Console.WriteLine("Answer: " + Encoding.UTF8.GetString(byteRecv, 0, bytes));
+                    if (msg.ToUpper() == "EXIT")
+                    {
+                        break;
+                    }
+                }
+                tcpClient.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
---
+++ HW_TCPServ_2/HW_TCPServ_2/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_TCPServ_2
+{
+    internal class TcpServ
+    {
+        static IPAddress locIp = IPAddress.Parse("127.0.0.1");
+        const int port = 11111;
+        static void Main(string[] args)
+        {
+            Dictionary<string, double> rate = new Dictionary<string, double>()
+            {
+                {"UAH", 4.0 },
+                {"USD", 40.0 },
+                {"EURO", 100.0 },
+                {"KZT", 0.1 }
+            };
+
+            TcpListener serv = new TcpListener(locIp, port);
+            serv.Start();
+            Console.WriteLine("Serv started");
+
+            Socket socket = serv.AcceptSocket();
+            Console.WriteLine("Connected");
+            StringBuilder sb = new StringBuilder();
+
+            try
+            {
+                byte[] bytesRec = new byte[1024];
+                int bytes = socket.Receive(bytesRec, bytesRec.Length, 0);
+                string dataRec = Encoding.UTF8.GetString(bytesRec, 0, bytes);
+
+                
+                sb.Append($"{dataRec}\n");
+                sb.Append($"Connected: {DateTime.Now.ToString()}");
+                sb.Append('\n');
+
+                while (true)
+                {
+                    bytes = socket.Receive(bytesRec, bytesRec.Length, 0);
+                    dataRec = Encoding.UTF8.GetString(bytesRec, 0, bytes);
+                    dataRec = dataRec.ToUpper();
+                    string resp = "";
+                    if (dataRec != "EXIT")
+                    {
+                        sb.Append(dataRec);
+                        sb.Append('\n');
+                        try
+                        {
+                            string[] split = dataRec.Split(' ');
+                            Console.WriteLine(split[0] + ' ' + split[1]);
+                            resp = $"{rate[split[0]] / rate[split[1]]}";
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex);
+                            resp = "ERROR: invalid format";
+                        }
+                    }
+                    else
+                    {
+                        resp = "disconnected";
+                        sb.Append($"Disconnected: {DateTime.Now.ToString()}");
+                        socket.Send(Encoding.UTF8.GetBytes(resp));
+                        break;
+                    }
+                    
+                    socket.Send(Encoding.UTF8.GetBytes(resp));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                sb.Append('\n');
+                File.WriteAllText("Log.txt" ,sb.ToString());
+                socket.Close();
+                serv.Stop();
+            }
+        }
+    }
+}
